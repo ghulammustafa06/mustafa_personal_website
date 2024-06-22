0b733fe3c97fbd7c3ec5168232c9fb0be743987a
Ghulam Mustafa <130176095+ghulammustafa06@users.noreply.github.com>
2024-06-22 17:51:19 +0500

    Add a Side Menu

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.querySelector('.sidebar');
+
+    window.showSidebar = () => sidebar.style.display = 'flex';
+    window.hideSidebar = () => sidebar.style.display = 'none';
+});
