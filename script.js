fbda67577370a7550fce9d2f498ccb37515693ac
Ghulam Mustafa <130176095+ghulammustafa06@users.noreply.github.com>
2024-06-23 20:03:54 +0500

    Add a social icons, image

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.querySelector('.sidebar');
-
     window.showSidebar = () => sidebar.style.display = 'flex';
     window.hideSidebar = () => sidebar.style.display = 'none';
 });
